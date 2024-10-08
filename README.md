54df1f82a89409a9f5507dec418dcc796034cca3
d3velopp <60269191+d3velopp@users.noreply.github.com>
2024-10-08 21:17:50 +0200

    new version Accept cookies

--- README.md
+++
@@ -1,2 +0,0 @@
-# Consentomatic
-project to create a browser extension to analyze cookies, accept or reject them, and rate the site on cookie service quality
--- popup.js
+++ popup.js
@@ -1,4 +1,39 @@
-document.getElementById("mon-bouton").addEventListener("click", function() {
-    alert("Vous avez cliqué !");
+document.getElementById("mon-bouton").addEventListener("click", async function() {
+  chrome.tabs.query({active: true, currentWindow: true}, async function(tabs) {
+      if (tabs.length > 0) {
+          const activeTab = tabs[0].id;
+
+
+          console.log("execution");
+          // Exécution du script après le délai
+          chrome.scripting.executeScript({
+            target: {tabId: activeTab},
+            func: detectCookieBanners
+          }, (results) => {
+            if (chrome.runtime.lastError) {
+              console.error(chrome.runtime.lastError);
+            } else {
+              console.log('Script exécuté avec succès', results);
+            }
+          });
+          
+      } else {
+          console.error("Aucun onglet actif trouvé.");
+      }
   });
-  
+});
+
+function detectCookieBanners() {
+  console.log("Détection des bannières de cookies dans la page...");
+  const banners = document.querySelectorAll('div, p, span');
+  banners.forEach(banner => {
+      if (banner.textContent.toLowerCase().includes('cookies')) {
+          //console.log('Bannière de cookies détectée :', banner);
+          const acceptButton = banner.querySelector('button, a');
+          if (acceptButton && acceptButton.textContent.toLowerCase().includes("les")) {
+              acceptButton.click();
+              console.log('Bouton "Accepter" cliqué.');
+          }
+      }
+  });
+}
