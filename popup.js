a24efe07d57c67d8ff5047b831705754a1148d40
Titouan <108216125+Titou2003@users.noreply.github.com>
2024-10-11 15:57:07 +0200

    Améliorations
    Améliorations de la détection de la fenêtre de cookies et amélioration de la detection des boutons accepter et refuser.

--- popup.js
+++ popup.js
@@ -6,7 +6,7 @@ document.getElementById("mon-bouton").addEventListener("click", async function()
           // Exécution du script après le délai
           chrome.scripting.executeScript({
             target: {tabId: activeTab},
-            func: detectCookieBanners
+            func: Consentomatic
           }, (results) => {
             if (chrome.runtime.lastError) {
               console.error(chrome.runtime.lastError);
@@ -21,39 +21,100 @@ document.getElementById("mon-bouton").addEventListener("click", async function()
   });
 });
 
-function detectCookieBanners() {
 
-  function getBanners(){
-    //return all banners containing the word "cookie"
-    console.log("Détection des bannières de cookies dans la page...");
-    const banners = document.querySelectorAll('div, p, span, button, a');
-    //console.log(banners.length);
-    return banners
+
+
+function Consentomatic() {
+
+  const AcceptButtonKeyWords = ['accepter', 'accept', 'allow'];
+  const DenyButtonKeyWords = ['refuser', 'refuse', 'sans accepter', 'deny', 'without', 'reject', 'decline'];
+  const CookieBannerKeyWords = ['cookie'];
+
+  function keyWordIsIncluded(keyword_list, element) {
+    result = false;
+    keyword_list.forEach(keyword => {
+      if ( element.textContent.toLowerCase().includes(keyword) ) {
+        result = true;
+      }
+    });
+    return result;
   }
 
-  function clickAcceptButton(banner){
+  function getCookieDivs(){ //RETURN COOKIE DIVS WITHOUT PARENT DIVS.
+    console.log("Détection des bannières de cookies dans la page...");
+    //get all elements of the page
+    const divs = document.querySelectorAll('div');
+    const cookieDivs = [];
+    console.log(divs.length);
+    //get all elements containing the word "cookie"
+    divs.forEach(element => {
+      //seach for the cookie banner div
+      if ( keyWordIsIncluded(CookieBannerKeyWords, element) ) {
+        console.log("cookie banner");
+        cookieDivs.push(element);
+      }
+    });
+    //get the element of the list that is at the maximal depth.
+    const elementToDelete = [];
+    cookieDivs.forEach(element => {
+      for (const child of element.children) {
+        if ( cookieDivs.includes(child)  && !(elementToDelete.includes(element)) ) {
+          elementToDelete.push(element);
+          console.log("delete parent");
+        }
+      }
+    });
+    elementToDelete.forEach(element => {
+      cookieDivs.pop(element);
+    });
+    console.log(cookieDivs.length);
+    cookieDivs.forEach(div => {
+      console.log(div.textContent);
+    });
+    return cookieDivs;
+  }
+
+  function clickButton(role, cookieBanners){
+    if (role == "ACCEPTER") { keyWord_list = AcceptButtonKeyWords; }
+    if (role == "REFUSER") { keyWord_list = DenyButtonKeyWords; }
     done = false;
-    const buttons = banner.querySelectorAll('button, a, span');
-    buttons.forEach(button => {
-      if ( done != true && button.textContent.toLowerCase().includes("accept")) {
-        button.click();
-        console.log("Bouton Accepter cliqué");
-        done = true;
+    cookieBanners.forEach(cookieBanner => {
+      //seach for the cookie banner div
+      if ( done != true ) {
+        const buttons = cookieBanner.querySelectorAll('button, a, span');
+        buttons.forEach(button => {
+          if (keyWordIsIncluded(AcceptButtonKeyWords, button) && button.textContent.length<60) {
+            button.click();
+            done = true;
+            console.log("Bouton cliqué");
+            console.log(button.textContent);
+          }
+        });
       }
     });
     return done;
   }
 
-  done = false;
-  banners=getBanners()
-  banners.forEach(banner => {
-      console.log("banner")
-      //seach for the cookie banner div
-      if ( done != true && banner.textContent.toLowerCase().includes('cookies')) {
-        console.log("cookie banner");
-        //now we are in the cookie banner div
-        done = clickAcceptButton(banner); //get the accept button and click
+  function widthSearchWithChildren(cookieDivs){
+    const sameOrLowerDepthElements = [];
+    cookieDivs.forEach(element => {
+      parent=element.parent;
+      for (const child of element.children) {
+        sameOrLowerDepthElements.push(element);
       }
-  });
+    });
+    cookieDivs.forEach(cookieBanner => {
+      for (const child of element.children) {
+        cookieDivs.push(child);
+      }
+    });
+    return sameOrLowerDepthElements;
+  }
+
+  done = false;
+  done = clickButton("ACCEPTER", getCookieDivs());
+  if (done) {
+    console.log("Terminé.");
+  }
 
 }
